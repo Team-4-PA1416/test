a6955bec14ac2c785541ea72e52a371169303279
Orkanen <Filip.v.antonsson@gmail.com>
2022-01-27 12:53:41 +0100

    v1.3
    Navbar integration to darkmode

--- src/css/NavbarElements.tsx
+++ src/css/NavbarElements.tsx
@@ -2,7 +2,6 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-    background: #63D471;
     height: 85px;
     display: flex;
     justify-content: space-between;
