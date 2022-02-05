3a8b9a9e3a170a2927163742219a469b26ae7028
Orkanen <Filip.v.antonsson@gmail.com>
2022-02-05 14:53:31 +0100

    v1.5
    Added Text format buttons.
    
    Working on: Exporting & Importing Themes that are currently in Navbar.tsx

--- src/view/components/About.tsx
+++ src/view/components/About.tsx
@@ -6,10 +6,32 @@ const About = () => {
             style={{
                 display: 'flex',
                 justifyContent: 'Center',
-                alignItems: 'Right'
+                alignItems: 'Right',
+                flexDirection: 'column',
+                textAlign: 'center',
             }}
         >
             <h1>About page.</h1>
+            <h2>Lorem Ipsum.</h2>
+            <h3>
+                "Neque porro quisquam est qui dolorem
+                ipsum quia dolor sit amet, consectetur, adipisci velit..."
+            </h3>
+            <h4>
+                "There is no one who loves pain itself,
+                 who seeks after it and wants to have it, simply because it is pain..."
+            </h4>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis dui eros, pharetra sed justo sit amet, dignissim varius justo.
+                Proin vestibulum nulla vitae nisl rhoncus posuere.
+                In id metus a est facilisis varius. Mauris ac mi sed felis
+                elementum tempus eget nec ante. Nam malesuada pulvinar lacus
+                quis tincidunt. Integer vestibulum blandit tortor eget hendrerit.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Pellentesque est lacus, mattis vel tincidunt finibus,
+                tempus eu magna.
+            </p>
         </div>
     );
 };
--- src/view/components/Navbar.tsx
+++ src/view/components/Navbar.tsx
@@ -7,7 +7,6 @@ import {
 import Button from 'react-bootstrap/button';
 import Navbar from 'react-bootstrap/navbar';
 
-
 const setDark = () => {
     localStorage.setItem("theme", "dark");
     document.documentElement.setAttribute("data-theme", "dark");
@@ -18,7 +17,18 @@ const setLight = () => {
     document.documentElement.setAttribute("data-theme", "light");
 };
 
+const setSizeOne = () => {
+    localStorage.setItem("size", "one");
+    document.documentElement.setAttribute("size-theme", "size-one");
+};
+
+const setSizeTwo = () => {
+    localStorage.setItem("size", "two");
+    document.documentElement.setAttribute("size-theme", "size-two");
+};
+
 const storedTheme = localStorage.getItem("theme");
+const storedSize = localStorage.getItem("size");
 
 const prefersDark =
     window.matchMedia &&
@@ -27,10 +37,16 @@ const prefersDark =
 const defaultDark =
     storedTheme === "dark" || (storedTheme === null && prefersDark);
 
+const defaultSize =
+    storedSize === "one" || (storedSize === null);
+
+if (defaultSize) {
+    setSizeOne();
+}
+
 if (defaultDark) {
     setDark();
 }
-
 
 const Navbarz = () => {
     return (
@@ -49,6 +65,10 @@ const Navbarz = () => {
                             Contact
                         </NavLink>
                         <NavBtn className="pull-right">
+                            <Button type="button"
+                                variant="light" onClick={setSizeOne}>c</Button>
+                            <Button type="button"
+                                variant="light" onClick={setSizeTwo}>C</Button>
                             <Button type="button"
                                 variant="light" onClick={setLight}>☀️</Button>
                             <Button type="button"
